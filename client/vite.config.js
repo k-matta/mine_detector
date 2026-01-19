4aa4ffb51365df84b5c0990c899757597261b1c0
ARandomDude789 <arandomdude789@outlook.com>
2026-01-19 13:15:01 -0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://0.0.0.0',
+        target: 'http://0.0.0.0:3000',
         changeOrigin: true,
         secure: false,
         ws: true,
@@ -17,4 +17,5 @@ export default defineConfig({
     },
   },
 });
+
 
