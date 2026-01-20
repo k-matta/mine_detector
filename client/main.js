fc92897b7340e8610a74371a30d2870ffd75bde1
ARandomDude789 <arandomdude789@outlook.com>
2026-01-20 17:09:38 -0500

    Update main.js

--- client/main.js
+++ client/main.js
@@ -2,20 +2,75 @@ import './style.css'
 import rocketLogo from '/rocket.png'
 import { DiscordSDK } from "@discord/embedded-app-sdk";
 
+let auth;
+
 const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
 
 setupDiscordSdk().then(() => {
 	console.log("Discord SDK ready.");
+
+	appendVoiceChannelName();
 });
 
 async function setupDiscordSdk() {
 	await discordSdk.ready();
+	console.log("Discord SDK ready.");
+
+	const { code } = await discordSdk.commands.authorize({
+		client_id: import.meta.VITE_DISCORD_CLIENT_ID,
+		response_type: "code",
+		state: "",
+		prompt: "none",
+		scope: [
+			"identify",
+			"guilds",
+			"applications.commands"
+		],
+	});
+	
+	const response = await fetch("/api/token", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			code
+		})
+	});
+	const { access_token } = await response.json();
+	
+	auth = await discordSdk.commands.authenticate({
+		access_token
+	});
+	
+	if (auth == null) {
+		throw new Error("Authenticate command failed");
+	}
+}
+
+async function appendVoiceChannelName() {
+	const app = document.getElementById("app");
+
+	let activityChannelName = "Unknown";
+
+	// Requesting the channel in GDMs (when the guild ID is null)
+	// Requires the dm_channels.read scope which requires DIscord approval
+	if (discordSdk.channelId != null && discordSdk.guildId != null) {
+		const channel = await discordSdk.commands.getChannel({channel_id: discordSdk.channelId});
+		if (channel.name != null) {
+			activityChannelName = channel.name;
+		}
+	}
+
+	const textTagString = `Activity Channel: "${activityChannelName}"`;
+	const textTag = document.createElement('p');
+	textTag.textContent = textTagString;
+	app.appendChild(textTag);
 }
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <img src="${rocketLogo}" class="logo" alt="Discord" />
-    <h1>Hello, World!</h1>
+	<img src="${rocketLogo}" class="logo" alt="Discord" />
+	<h1>Hello, World!</h1>
   </div>
-
 `;
