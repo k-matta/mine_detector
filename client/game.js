31b3e6797c94b7a71cf42b8d43140db29904dd08
mattak1 <mattak1@mcmaster.ca>
2026-05-21 19:22:17 -0400

    Fixing socket request path

--- client/game.js
+++ client/game.js
@@ -1,4 +1,4 @@
-import { io } from "https://cdn.socket.io/4.8.3/socket.io.esm.min.js";
+import { io } from "/socket";
 
 let codeElement = document.getElementById("code");
 const code = codeElement.innerText;
