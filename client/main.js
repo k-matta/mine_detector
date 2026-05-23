738e0b4590b486b8b5a5b98ea32281f3901b4cae
mattak1 <mattak1@mcmaster.ca>
2026-05-22 21:50:25 -0400

    Attempting to add the script via appendChild()

--- client/main.js
+++ client/main.js
@@ -4,6 +4,43 @@ import { DiscordSDK } from "@discord/embedded-app-sdk";
 try {
 	let auth;
 	const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+	
+	async function setupDiscordSdk() {
+		await discordSdk.ready();
+		const { code } = await discordSdk.commands.authorize({
+			client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
+			response_type: "code",
+			state: "",
+			prompt: "none",
+			scope: [
+				"identify",
+				"guilds",
+				"applications.commands"
+			],
+		});
+		const response = await fetch("/api/token", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				code
+			})
+		});
+		
+		const json = await response.json();
+		// const { access_token } = await response.json();
+		const access_token = json.access_token;
+		auth = await discordSdk.commands.authenticate({
+			access_token
+		});
+		
+		if (auth == null) {
+			throw new Error("Authenticate command failed");
+		}
+		console.log(code)
+		return code;
+	}
 	setupDiscordSdk().then((code) => {
 		document.getElementById("title").hidden = true;
 		document.body.innerHTML += `
@@ -53,47 +90,11 @@ try {
 				<button type="button" id="return">Return to Menu</button>
 			</section>
 		</section>
-		<script src="/assets/game.js" crossorigin type="module"></script>
 		<div id="code" hidden>${code}</div>`
-		const script = document.body.lastChild;
+		const script = document.createElement("script");
+		script.src = "/assets/game.js";
+		document.body.appendChild(script);
 	});
-
-	async function setupDiscordSdk() {
-		await discordSdk.ready();
-		const { code } = await discordSdk.commands.authorize({
-			client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
-			response_type: "code",
-			state: "",
-			prompt: "none",
-			scope: [
-				"identify",
-				"guilds",
-				"applications.commands"
-			],
-		});
-		const response = await fetch("/api/token", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				code
-			})
-		});
-
-		const json = await response.json();
-		// const { access_token } = await response.json();
-		const access_token = json.access_token;
-		auth = await discordSdk.commands.authenticate({
-			access_token
-		});
-		
-		if (auth == null) {
-			throw new Error("Authenticate command failed");
-		}
-		console.log(code)
-		return code;
-	}
 } catch(e) {
 	console.log(e)
 	document.body.innerHTML += "<p style='margin:20px; padding: 10px; background-color: #500; border-radius: 5px; font-size: 0.5em;'>Uh oh! There was an error authenticating this client. Please close and reopen the game to try again.<br>If the problem persists, please contact support.</p>";
