ecde0c7ed61a276958bf32587c625ac7db4dc35f
mattak1 <mattak1@mcmaster.ca>
2026-05-31 18:00:47 -0400

    Adding "random" generation to the server

--- client/game.js
+++ client/game.js
@@ -312,15 +312,6 @@ function createInnerBoard(size, board) {
 	}
 }
 
-function mulberry32(seed) {
-	return function() {
-		let t = (seed += 0x6D2B79F5);
-		t = Math.imul(t ^ (t >>> 15), t | 1);
-		t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
-		return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
-	};
-}
-
 /**
  * Generates a game board with a specified size, number of mines, and, optionally, a seed.
  * @param {Number} size X and Y dimension of game board
--- server/game.js
+++ server/game.js
@@ -192,3 +192,12 @@ export class GridItem {
 		return this.covered;
 	}
 }
+
+function mulberry32(seed) {
+	return function() {
+		let t = (seed += 0x6D2B79F5);
+		t = Math.imul(t ^ (t >>> 15), t | 1);
+		t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+		return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+	};
+}
